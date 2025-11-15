deb76df7bb6bcffa54b4f07f5047b648abb1d82a
mugni1 <asepabdulmugni22@gmail.com>
2025-11-15 21:50:46 +0700

    feat: add handle-multer-error middleware

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,5 +1,8 @@
 import { authorizationMiddleware } from './authorization/authorization.middleware';
+import { uploadMiddleware, handleMulterErrorMiddleware } from './upload/upload.middleware';
 
 export {
   authorizationMiddleware,
+  uploadMiddleware,
+  handleMulterErrorMiddleware,
 }
---
+++ src/middlewares/upload/upload.middleware.ts
@@ -0,0 +1,14 @@
+import { NextFunction, Request, Response } from "express";
+import multer from "multer";
+import { response } from "../../../utils/response";
+
+const storage = multer.memoryStorage();
+
+export const uploadMiddleware = multer({ storage, limits: { fileSize: 1024 * 1024 * 5 } });
+
+export const handleMulterErrorMiddleware = (err: any, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof multer.MulterError) {
+    return response({ message: err.message, res, status: 400 });
+  }
+  next();
+};
