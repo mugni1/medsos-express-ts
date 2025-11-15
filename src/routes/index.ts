918f11d7dab619f51ddb7a8f98d955c0d5569b10
mugni1 <asepabdulmugni22@gmail.com>
2025-11-15 21:51:39 +0700

    feat: define upload router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import profileRoute from "./profile/profile.route";
 import authRoute from "./auth/auth.route";
 import userRoute from "./user/user.route";
+import uploadRoute from "./upload/upload.route";
 
 export {
   profileRoute,
   authRoute,
-  userRoute
+  userRoute,
+  uploadRoute
 }
