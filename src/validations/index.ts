7b25151220810dcb9d031a2b8e4d193a086b9979
mugni1 <asepabdulmugni22@gmail.com>
2025-11-15 19:57:30 +0700

    feat: add update profile validation

--- src/validations/index.ts
+++ src/validations/index.ts
@@ -1,6 +1,8 @@
 import { registerSchema, loginSchema } from "./auth/auth.validate";
+import { updateProfileSchema } from "./profile/profile.validate";
 
 export {
   loginSchema,
-  registerSchema
+  registerSchema,
+  updateProfileSchema
 }
