e7ec5e6c90c5d079d4b702592b57379b6738916a
mugni1 <asepabdulmugni22@gmail.com>
2025-11-15 19:58:16 +0700

    feat: add update profile route

--- src/routes/profile/profile.route.ts
+++ src/routes/profile/profile.route.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
-import { getProfile } from '../../controllers';
+import { getProfile, updateProfile } from '../../controllers';
 import { authorizationMiddleware } from '../../middlewares';
 
 const router = Router();
 router.get(`/profile`, authorizationMiddleware, getProfile);
+router.put(`/profile`, authorizationMiddleware, updateProfile);
 
 export default router;
