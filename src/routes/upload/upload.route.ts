23769e64edbe3bab1fa8f7c17b797cdad420aff0
mugni1 <asepabdulmugni22@gmail.com>
2025-11-16 07:37:50 +0700

    refactor: add authmiddleware on router

--- src/routes/upload/upload.route.ts
+++ src/routes/upload/upload.route.ts
@@ -1,9 +1,9 @@
 import { Router } from "express";
 import { deleteFile, uploadFile } from "../../controllers";
-import { handleMulterErrorMiddleware, uploadMiddleware } from "../../middlewares";
+import { authorizationMiddleware, handleMulterErrorMiddleware, uploadMiddleware } from "../../middlewares";
 
 const router = Router();
-router.post("/upload", uploadMiddleware.single("file"), handleMulterErrorMiddleware, uploadFile);
-router.delete("/delete", deleteFile);
+router.post("/upload", authorizationMiddleware, uploadMiddleware.single("file"), handleMulterErrorMiddleware, uploadFile);
+router.delete("/delete", authorizationMiddleware, deleteFile);
 
 export default router;
