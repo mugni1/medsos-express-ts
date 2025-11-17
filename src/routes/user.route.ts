df1835a0f464716bae9bef0486035e95d73d3687
mugni1 <asepabdulmugni22@gmail.com>
2025-11-17 14:37:58 +0700

    refactor: change import get-random-user

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,10 +1,10 @@
 import { Router } from 'express';
-import { getUserByUsername, getUsers, getRandomUsers } from '@/controllers';
+import { getUserByUsername, getUsers, getUsersRandom } from '@/controllers';
 import { authorizationMiddleware } from '@/middlewares';
 
 const router = Router();
 router.get("/user", authorizationMiddleware, getUsers);
-router.get("/user-random", authorizationMiddleware, getRandomUsers);
+router.get("/user-random", authorizationMiddleware, getUsersRandom);
 router.get("/user/:username", authorizationMiddleware, getUserByUsername);
 
 export default router;
